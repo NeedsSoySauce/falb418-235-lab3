95ed92248bb32a86b71a52d21029307c1fc1faa5
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-08-19 21:09:21 +1200

    Added tests for valid moves

--- test_robot.py
+++ test_robot.py
@@ -82,6 +82,35 @@ def test_illegal_move_north(robot):
             robot.move()
 
 
+def test_move_east(robot):
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 2
+
+
+def test_move_south(robot):
+    robot.move()
+    robot.turn()
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
+def test_move_west(robot):
+    robot.turn()
+    robot.move()
+    robot.turn()
+    robot.turn()
+    robot.move()
+    state = robot.state()
+    assert state['row'] == 10
+    assert state['col'] == 1
+
+
 def test_move_north(robot):
     robot.move()
     state = robot.state()
